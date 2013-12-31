8dcb11802333a5f8794e96ac69561176bc8acdff
Mat Whitney <mwhitney@mail.sdsu.edu>
2013-12-31 11:55:51 -0800

    Moved common functionality back to form library
    There are a couple of items that will likely need to be handled by the
    driver, but most of this functionality is independent of the driver. See
    @todo lines in form library

--- bonfire/libraries/form.php
+++ bonfire/libraries/form.php
@@ -169,7 +169,7 @@ class Form
      */
     public static function form_helper_common($helperArgs=array())
     {
-        return self::$form->form_helper_common($helperArgs);
+        return self::buildControl($helperArgs);
     }
 
 	//--------------------------------------------------------------------
@@ -183,7 +183,9 @@ class Form
      */
     public static function form_helper_dropdown($helperArgs)
     {
-        return self::$form->form_helper_dropdown($helperArgs);
+        $helperArgs['type'] = 'select';
+
+        return self::buildControl($helperArgs);
     }
 
 	//--------------------------------------------------------------------
@@ -201,7 +203,14 @@ class Form
 	 */
 	public static function field($name, $properties=array())
 	{
-		return self::$form->field($name, $properties);
+		if ( ! isset($properties['name'])) {
+			$properties['name'] = $name;
+		}
+        if (isset($properties['help'])) {
+            $properties['tooltip'] = $properties['help'];
+        }
+
+        return self::buildControl($properties);
 	}//end field()
 
 	//--------------------------------------------------------------------
@@ -219,7 +228,22 @@ class Form
 	 */
 	public static function label($value, $options=null)
 	{
-		return self::$form->label($value, $options);
+        if (is_string($options)) {
+            $options = array('for' => $options);
+        }
+
+        // This check is after is_string() because some string values are
+        // considered empty, but may be valid labels
+        if (empty($options)) {
+            $options = array();
+        }
+
+        $settings = array(
+            'value'         => $value,
+            'attributes'    => $options,
+        );
+
+        return self::buildLabel($settings);
 	}//end label()
 
 	//--------------------------------------------------------------------
@@ -237,7 +261,27 @@ class Form
 	 */
 	public static function input($options, $extended=false)
 	{
-		return self::$form->input($options, $extended);
+        if ($extended) {
+            return self::buildControl($options);
+        }
+
+		if ( ! isset($options['type'])) {
+			logit('You must specify a type for the input.');
+		} elseif ( ! in_array($options['type'], self::$standard_inputs)) {
+			logit(sprintf('"%s" is not a valid input type.', $options['type']));
+		}
+
+        $settings = array(
+            'data'  => $options,
+            'extra' => '',
+        );
+
+        if (isset($options['extra'])) {
+            $settings['extra'] = $options['extra'];
+            unset($settings['data']['extra']);
+        }
+
+        return self::buildInput($settings);
 	}//end input()
 
 	//--------------------------------------------------------------------
@@ -255,7 +299,23 @@ class Form
 	 */
 	public static function textarea($options, $extended=false)
 	{
-		return self::$form->textarea($options, $extended);
+        if ($extended) {
+            $options['type'] = 'textarea';
+
+            return self::buildControl($options);
+        }
+
+        $settings = array(
+            'data'  => $options,
+            'extra' => '',
+        );
+
+        if (isset($options['extra'])) {
+            $settings['extra'] = $options['extra'];
+            unset($settings['data']['extra']);
+        }
+
+        return self::buildTextarea($settings);
 	}//end textarea()
 
 	//--------------------------------------------------------------------
@@ -273,7 +333,16 @@ class Form
 	 */
 	public static function state($options, $extended=false)
 	{
-        return self::$form->state($options, $extended);
+        if ($extended) {
+            $options['type'] = 'state';
+
+            return self::buildControl($options);
+        }
+
+		$options['name']    = isset($options['name']) ? $options['name'] : '';
+		$options['value']   = isset($options['value']) ? $options['value'] : '';
+
+        return self::buildStateSelect($options);
 	}//end state()
 
 	//--------------------------------------------------------------------
@@ -332,6 +401,239 @@ class Form
 		return implode(' ', $attributes);
 
 	}//end attr_to_string()
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Build a form control, complete with label, help/error text, etc.
+     *
+     * @todo Formatting of errors should be handled by the driver
+     * @todo $label['attributes'] should be handled by the driver
+     * @todo Templates should probably be more flexible
+     *
+     * @param Array $options The settings used to build the control, generally handles the inputs passed to BF_form_helper's functions
+     *
+     * @return String    The HTML to display the control
+     */
+    protected static function buildControl($options)
+    {
+        $settings = array(
+            'data'      => '',
+            'extra'     => '',
+            'label'     => '',
+            'name'      => '',
+            'tooltip'   => '',
+            'type'      => 'text',
+            'value'     => '',
+        );
+
+        // Make sure we don't overwrite a default value with empty data
+        foreach ($settings as $key => $val) {
+            if (isset($options[$key]) && empty($options[$key])) {
+                unset($options[$key]);
+            }
+        }
+
+        $settings = array_merge($settings, $options);
+
+        /*
+         * If $settings['data'] is not an array, it should be a string
+         * containing the name of the control, so we turn it into an array and
+         * assign its value to the 'name' key.
+         */
+        if ( ! is_array($settings['data'])) {
+            $settings['data'] = array('name' => $settings['data']);
+        }
+
+        /*
+         * If $settings['extra'], $settings['label'], $settings['name'], or
+         * $settings['tooltip'] is empty, try to retrieve the value(s) from
+         * $settings['data']
+         */
+        if (empty($settings['extra']) && isset($settings['data']['extra'])) {
+            $settings['extra'] = $settings['data']['extra'];
+            unset($settings['data']['extra']);
+        }
+
+        if (empty($settings['label']) && isset($settings['data']['label'])) {
+            $settings['label'] = $settings['data']['label'];
+            unset($settings['data']['label']);
+        }
+
+        if (empty($settings['name']) && isset($settings['data']['name'])) {
+            $settings['name'] = $settings['data']['name'];
+            // We don't unset the name, because it is required for form fields
+        }
+
+        if (empty($settings['tooltip']) && isset($settings['data']['tooltip'])) {
+            $settings['tooltip'] = $settings['data']['tooltip'];
+            unset($settings['data']['tooltip']);
+        }
+
+        if ($settings['name'] != $settings['data']['name']) {
+            $settings['data']['name'] = $settings['name'];
+        }
+
+        /*
+         * If there is an error message, add it to the tooltip
+         */
+        $error = '';
+        if (function_exists('form_error')) {
+            if (form_error($settings['name'])) {
+                $error = ' error';
+                $settings['tooltip'] = form_error($settings['name']) . '<br />' . $settings['tooltip'];
+            }
+        }
+
+        /*
+         * We may want to change the label attributes (or value) for some
+         * controls
+         */
+        $label = array(
+            'value' => $settings['label'],
+            'attributes' => array(
+                'class' => 'control-label',
+            ),
+        );
+
+        // Build the control itself based on the value of $settings['type']
+        $input = '';
+        switch ($settings['type']) {
+            case 'dropdown':
+            case 'select':
+                $input = self::buildSelect($settings);
+                break;
+
+            case 'textarea':
+                $input = self::buildTextarea($settings);
+                break;
+
+            case 'state':
+                $input = self::buildStateSelect($settings);
+                break;
+
+            default:
+                $input = self::buildInput($settings);
+                break;
+        }
+
+        /*
+         * A label's for attribute should always be set to the id value of the
+         * control, so if we don't have an id, we don't set the for attribute
+         */
+        if ( ! empty($settings['id'])) {
+            $label['attributes']['for'] = $settings['id'];
+        }
+        if (empty($label['attributes']['for']) && ! empty($settings['data']['id'])) {
+            $label['attributes']['for'] = $settings['data']['id'];
+        }
+
+        /*
+         * Finally, build the label and replace the placeholders in the template
+         */
+        $search = array('{label}', '{input}', '{help}', '{error_class}');
+        $replace = array(
+            self::buildLabel($label),
+            $input,
+            $settings['tooltip'],
+            $error,
+        );
+
+		return str_replace($search, $replace, self::getTemplate());
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * @internal Driver methods
+     *
+     * The methods below (build*, except for buildControl, above, and
+     * getTemplate) are required to be implemented in any fully-functional
+     * driver
+     *
+     */
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Build an HTML input control
+     *
+     * @param Array $settings The settings used to build the control
+     *
+     * @return String    The HTML input
+     */
+    protected static function buildInput($settings)
+    {
+        return self::$form->buildInput($settings);
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Build an HTML label
+     *
+     * @param Array $settings The settings used to build the label (String 'value' and Array 'attributes')
+     *
+     * @return String    The HTML label
+     */
+    protected static function buildLabel($settings)
+    {
+        return self::$form->buildLabel($settings);
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Build an HTML select (dropdown) control
+     *
+     * @param Array $settings The settings used to build the control
+     *
+     * @return String    The HTML select
+     */
+    protected static function buildSelect($settings)
+    {
+        return self::$form->buildSelect($settings);
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Build a State Select control
+     *
+     * @param Array $settings The settings used to build the control
+     *
+     * @return String    The HTML for the state select
+     */
+    protected static function buildStateSelect($settings)
+    {
+        return self::$form->buildStateSelect($settings);
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Build an HTML textarea control
+     *
+     * @param Array $settings The settings used to build the control
+     *
+     * @return String    The HTML for the textarea
+     */
+    protected static function buildTextarea($settings)
+    {
+        return self::$form->buildTextarea($settings);
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * Get the template (because we have static interference)
+     *
+     * @return String    The template
+     */
+    protected static function getTemplate()
+    {
+        return self::$form->getTemplate();
+    }
 
 	//--------------------------------------------------------------------
 
